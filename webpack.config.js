32a821bee5802b8b53efb952d8b40cddbdc13463
schibrach <sandieschibrach@gmail.com>
2026-05-05 22:16:14 +0200

    Uppdaterade .gitignore, package.json, package-lock.json och webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
